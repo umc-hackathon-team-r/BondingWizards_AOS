5ef18ca465ac23698fe87c0fe6044d738aacae6e
dokyung-kang <kangdk02@naver.com>
2024-01-03 06:04:01 +0900

    [update] 앱 알림 임시 데이터 변경

--- app/src/main/java/com/umc/bondingwizards/presentation/activity/NotificationFragment.kt
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/NotificationFragment.kt
@@ -15,8 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class NotificationFragment: BindingFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
 
     private val notiList = listOf(
-        Notification(0, "테디 생일", "테디에게 생일 선물을 보내세요"),
-        // Add more alarms as needed
+        Notification(0, "테디 생일", "테디님의 생일에 대한 정보를 확인해보세요."),
+        Notification(1, "새해", "오늘은 새해 첫날입니다. 소중한 사람들에게 메세지를 보내보시는건 어떠신가요?"),
     )
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
