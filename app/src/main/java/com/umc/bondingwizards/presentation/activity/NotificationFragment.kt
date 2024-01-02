21cae1c6ab911c8f1ec0d267d95ba926c614b7a4
dokyung-kang <kangdk02@naver.com>
2024-01-03 05:46:55 +0900

    [add] 알림 페이지

--- app/src/main/java/com/umc/bondingwizards/presentation/activity/NotificationFragment.kt
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/NotificationFragment.kt
@@ -3,19 +3,35 @@ package com.umc.bondingwizards.presentation.activity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.bondingwizards.R
 import com.umc.bondingwizards.databinding.FragmentNotificationBinding
+import com.umc.bondingwizards.domain.model.Notification
+import com.umc.bondingwizards.presentation.adapter.NotificationAdapter
 import com.umc.bondingwizards.utils.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NotificationFragment: BindingFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
 
+    private val notiList = listOf(
+        Notification(0, "테디 생일", "테디에게 생일 선물을 보내세요"),
+        // Add more alarms as needed
+    )
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.toolbar.btnToolbarBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        setRecyclerView()
+        binding.executePendingBindings()
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun setRecyclerView(){
+        val notificationAdapter = NotificationAdapter(notiList)
+        binding.rvNotiList.adapter = notificationAdapter
+        binding.rvNotiList.layoutManager = LinearLayoutManager(requireContext())
     }
 }
