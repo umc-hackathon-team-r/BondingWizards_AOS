ef7ba1abd6378500c62f5a842d8d3ea734fb277c
NaZe0320 <rwt1226@naver.com>
2024-01-03 00:26:20 +0900

    [add] 글 추천 페이지 ui 구성

--- app/src/main/java/com/umc/bondingwizards/presentation/activity/RecommendWriting2Fragment.kt
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/RecommendWriting2Fragment.kt
@@ -1,7 +1,6 @@
 package com.umc.bondingwizards.presentation.activity
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
@@ -10,18 +9,19 @@ import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
 import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentRecommandWriting2Binding
 import com.umc.bondingwizards.databinding.FragmentRecommandWritingBinding
 import com.umc.bondingwizards.presentation.adapter.FlexBoxAdapter
 import com.umc.bondingwizards.utils.binding.BindingFragment
 
-class RecommendWritingFragment : BindingFragment<FragmentRecommandWritingBinding>(R.layout.fragment_recommand_writing){
+class RecommendWriting2Fragment : BindingFragment<FragmentRecommandWriting2Binding>(R.layout.fragment_recommand_writing2){
 
     private lateinit var flexBoxAdapter: FlexBoxAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val buttonTextList = listOf("새해","생일","결혼기념일","명절","크리스마스","커플")
+        val buttonTextList = listOf("친근한","전문적인","감사의","낭만적인","유머 있는","창의적인")
 
         flexBoxAdapter = FlexBoxAdapter(buttonTextList) { buttonText, selected ->
             // 버튼 클릭 이벤트 처리
@@ -44,11 +44,13 @@ class RecommendWritingFragment : BindingFragment<FragmentRecommandWritingBinding
         }
 
         binding.btnNext.setOnClickListener {
-            Log.d("TEST" ,"tlqkf")
             findNavController().navigate(
-                R.id.action_recommendWritingFragment_to_recommendWriting2Fragment
+                R.id.action_recommendWriting2Fragment_to_recommendWriting3Fragment2
             )
         }
 
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
---
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/RecommendWriting3Fragment.kt
@@ -0,0 +1,23 @@
+package com.umc.bondingwizards.presentation.activity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentRecommandWriting2Binding
+import com.umc.bondingwizards.databinding.FragmentRecommandWriting3Binding
+import com.umc.bondingwizards.databinding.FragmentRecommandWritingBinding
+import com.umc.bondingwizards.presentation.adapter.FlexBoxAdapter
+import com.umc.bondingwizards.utils.binding.BindingFragment
+
+class RecommendWriting3Fragment : BindingFragment<FragmentRecommandWriting3Binding>(R.layout.fragment_recommand_writing3){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
