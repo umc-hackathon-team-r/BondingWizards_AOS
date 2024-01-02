21bc7ab748ff362c7b863aa1e7d2ee50de62b5e1
NaZe0320 <rwt1226@naver.com>
2024-01-02 19:15:31 +0900

    [add] 친구 목록 아이템 구성

---
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/ListFragment.kt
@@ -0,0 +1,11 @@
+package com.umc.bondingwizards.presentation.activity
+
+import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentListBinding
+import com.umc.bondingwizards.utils.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListFragment: BindingFragment<FragmentListBinding>(R.layout.fragment_list) {
+
+}
---
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/ListFriendFragment.kt
@@ -0,0 +1,9 @@
+package com.umc.bondingwizards.presentation.activity
+import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentFriendListBinding
+import com.umc.bondingwizards.utils.binding.BindingFragment
+
+class ListFriendFragment: BindingFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list) {
+
+
+}
