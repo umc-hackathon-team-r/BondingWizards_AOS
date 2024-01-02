291dfaafc3cb5105f996e1c1883404ecd9bbae1e
NaZe0320 <rwt1226@naver.com>
2024-01-02 15:59:40 +0900

    [add] CalendarFragment 구성

---
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/CalendarFragment.kt
@@ -0,0 +1,11 @@
+package com.umc.bondingwizards.presentation.activity
+
+import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentCalendarBinding
+import com.umc.bondingwizards.utils.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CalendarFragment: BindingFragment<FragmentCalendarBinding>(R.layout.fragment_calendar) {
+
+}
