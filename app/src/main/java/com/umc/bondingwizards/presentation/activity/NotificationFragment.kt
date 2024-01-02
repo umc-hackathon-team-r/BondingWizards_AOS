6ac59234447bbcdb76b0aff1be52f30c9e78cf08
NaZe0320 <rwt1226@naver.com>
2024-01-02 16:59:46 +0900

    [add] 알림페이지 구성

---
+++ app/src/main/java/com/umc/bondingwizards/presentation/activity/NotificationFragment.kt
@@ -0,0 +1,10 @@
+package com.umc.bondingwizards.presentation.activity
+
+import com.umc.bondingwizards.R
+import com.umc.bondingwizards.databinding.FragmentNotificationBinding
+import com.umc.bondingwizards.utils.binding.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NotificationFragment: BindingFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+}
